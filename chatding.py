2a81cca2e26c386852bc276b745fd294fb38d630
Artin0123 <artinlintw@gmail.com>
2024-07-16 21:20:01 +0800

    disable ctrl c

--- chatding.py
+++ chatding.py
@@ -7,10 +7,9 @@ sock = socket.socket()
 sock.connect(('irc.chat.twitch.tv', 6667))
 sock.send(f"NICK justinfan0\n".encode('utf-8'))
 sock.send(f"JOIN {CHANNEL}\n".encode('utf-8'))
-sock.setblocking(0)  # 設置 socket 為非阻塞模式
+# sock.setblocking(0)  # 設置 socket 為非阻塞模式
 
 lastAlert = 0
-running = True
 
 def parseChat(resp):
     resp = resp.rstrip().split('\r\n')
@@ -21,29 +20,31 @@ def parseChat(resp):
             line = user + ": " + msg
         print(line)
 
-try:
-    while running:
-        try:
-            resp = sock.recv(2048).decode('utf-8')
+# try:
+while True:
+    # try:
+    resp = sock.recv(2048).decode('utf-8')
 
-            if resp.startswith('PING'):
-                sock.send("PONG\n".encode('utf-8'))
-            
-            elif len(resp) > 0:
-                parseChat(resp)
-                if not window.foreground() and time.time() - lastAlert > DELAY:
-                    if ALERT_SOUND: 
-                        sound.alert()
-                        print(SOUND_FILE)
-                    if ALERT_RUMBLE: rumble.alert()
-                    lastAlert = time.time()
-        
-        except BlockingIOError:
-            # 當沒有數據可讀時，捕獲 BlockingIOError 並繼續迴圈
-            pass
-        
-        time.sleep(1)  # 每1秒檢查一次
-except KeyboardInterrupt:
-    running = False
-    print("Program exited gracefully")
-    sock.close()
+    if resp.startswith('PING'):
+        sock.send("PONG\n".encode('utf-8'))
+    
+    elif len(resp) > 0:
+        parseChat(resp)
+        if not window.foreground() and time.time() - lastAlert > DELAY:
+            if ALERT_SOUND: 
+                sound.alert()
+                # print(SOUND_FILE)
+            if ALERT_RUMBLE: rumble.alert()
+            lastAlert = time.time()
+    
+    # except BlockingIOError:
+        # 當沒有數據可讀時，捕獲 BlockingIOError 並繼續迴圈
+        # pass
+    
+    time.sleep(1)  # 每1秒檢查一次
+    # pass
+
+# except KeyboardInterrupt:
+#     print("Program exited gracefully")
+#     sock.close()
+    
