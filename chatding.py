59e5fd7515eeca42141c46b027fc4bff39aefa0f
Artin0123 <artinlintw@gmail.com>
2024-07-17 13:34:03 +0800

    change channel name in console

--- chatding.py
+++ chatding.py
@@ -1,7 +1,26 @@
 import socket, time
-from config import DELAY, CHANNEL, ALERT_SOUND, ALERT_RUMBLE, SOUND_FILE
+from config import DELAY, CHANNEL, ALERT_SOUND, ALERT_RUMBLE, BASE_DIR, SOUND_FILE
 from alerts import sound, rumble
 from common import window
+import os
+
+print("First time setting")
+# 假設 BASE_DIR 已經在 config.py 中定義
+channel_file_path = os.path.join(BASE_DIR, 'channel_name.txt')
+
+# 檢查 channel_name.txt 檔案是否存在
+if not os.path.exists(channel_file_path):
+    # 如果檔案不存在，提示用戶輸入頻道名稱
+    channel_name = input("Input Twitch channel name: ")
+    # 將頻道名稱寫入檔案
+    with open(channel_file_path, 'w') as file:
+        file.write(channel_name)
+    # 更新 CHANNEL 變數
+    CHANNEL = '#' + channel_name
+else:
+    # 如果檔案存在，從檔案讀取頻道名稱
+    with open(channel_file_path, 'r') as file:
+        CHANNEL = '#' + file.read().strip()
 
 sock = socket.socket()
 sock.connect(('irc.chat.twitch.tv', 6667))
